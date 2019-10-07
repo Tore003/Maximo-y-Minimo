b4f598630a8672d1a9fccdb621fc195d4fb52926
Tore003 <56234021+Tore003@users.noreply.github.com>
2019-10-06 22:43:25 -0300

    Update MaxMin.java

--- MaxMin.java
+++ MaxMin.java
@@ -2,33 +2,27 @@ import java.io.*;
 
 public class MaxMin {
 
-    
     public static void main(String[] args) throws IOException {
         BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
         int numero;
-        int nummax=0;
-        int nummin=0;
+        int nummax = 0;
+        int nummin = 999999999;
         System.out.println("Ingrese un numero (Si el numero ingresado es cero, cierra la carga)");
-        numero = Integer.parseInt(bufEntrada.readLine());
+        do {
+            numero = Integer.parseInt(bufEntrada.readLine());
+            if (numero == 0) {
+                System.out.println("Carga terminada");
+            } else {
+                if (numero > nummax) {
+                    nummax = numero;
+                }
+                if (numero < nummin) {
+                    nummin = numero;
+                }
+            }
+        } while (numero > 0);
 
-        while(numero>0)
-        {
-            if(numero==0)
-        {
-            System.out.println("Carga terminada");
-        }
-            nummax=numero;
-            if(numero>nummax)
-            {
-                nummax=numero;
-            }
-            if(numero<nummin)
-            {
-                nummin=numero;
-            }
-        }
-        System.out.println("El valor maximo es: " +nummax);
-        System.out.println("El valor minimo es: " +nummin);
-        
+        System.out.println("El valor maximo es: " + nummax);
+        System.out.println("El valor minimo es: " + nummin);
     }
 }
